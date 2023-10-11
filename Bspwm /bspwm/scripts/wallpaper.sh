5f7c805e762b0ba45f8cf2be7d72cf46f6703ab7
Jesuarzm <jesuarzambrano@hotmail.com>
2023-10-11 07:58:40 -0600

    Add wallpaper scripts

---
+++ Bspwm /bspwm/scripts/wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+nitrogen --random --set-scaled --head=0 '/home/jesuar/Imágenes/wallpapers_animes'
---
+++ Bspwm /bspwm/scripts/wallpaper_h.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+nitrogen --random --set-scaled --head=0 '/run/media/jesuar/2TB/.Pics/Wallpaper'
